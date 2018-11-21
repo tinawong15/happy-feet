cdd8ebd53a339425cc5632bd04618318a30e2e4e
Rubin Peci <rubinpeci@gmail.com>
2018-11-20 23:10:34 -0500

    more work on api and fix design doc

--- util/news.py
+++ util/news.py
@@ -6,8 +6,19 @@ API_KEY = "a842f08935ec4c4f8cbfa0ca729fc2c1"
 def top_headlines_by_src(src):
     url = ('https://newsapi.org/v2/top-headlines?'
        'sources=' + src + '&'
+       'language=en&'
        'apiKey=' + API_KEY)
-    response = requests.get(url)
-    return json.loads(response.text)
+    response = urllib.request.urlopen(url)
+    return json.loads(response.read())
 
 def top_headlines_by_keyword(keyword):
+    url = ('https://newsapi.org/v2/top-headlines?'
+        'q=' + keyword + '&'
+        'sortBy=popularity&'
+        'language=en&'
+       'apiKey=' + API_KEY)
+    response = urllib.request.urlopen(url)
+    return json.loads(response.read())
+
+#debug print statements
+#print(top_headlines_by_keyword('Apple'))
