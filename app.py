802419dc3c664bd9449178435eebaa0e34c1c031
Rubin Peci <rubinpeci@gmail.com>
2018-11-22 10:24:53 -0500

    Add simple news api test features. Works as intended

--- app.py
+++ app.py
@@ -7,5 +7,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    articles = news.list_article_titles(news.top_headlines_by_keyword('bitcoin'))
-    return render_template('home.html', list = articles) 
+    raw = news.top_headlines_by_keyword('bitcoin')
+    articles = news.list_article_titles(raw)
+    links = news.list_article_urls(raw)
+    dictionary = {}
+
+    i = 0
+    while (i < len(articles)):
+        dictionary[articles[i]] = links[i]
+        i += 1
+    
+    print(dictionary)
+
+    return render_template('home.html', data = dictionary) 
