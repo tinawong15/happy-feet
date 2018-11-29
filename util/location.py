e7298d1b73c247cb9a7df4ddf69be0d70dce6f53
Rubin Peci <rubinpeci@gmail.com>
2018-11-29 13:20:31 -0500

    get bings map api working uwu

--- util/location.py
+++ util/location.py
@@ -1,10 +1,17 @@
 import json, urllib.request
 
-API_LINK = ' https://api.openstreetmap.org/search'
+API_LINK = 'http://dev.virtualearth.net/REST/v1/Locations?query='
+API_KEY = 'AiIs8nS3kIqOkEJUD9uKnIpk4WecoekWcF39HhFxfaCutK2c652aCU5nqwAaeWFf'
 
 def get_raw_data(location):
-    url = API_LINK + 'q=' + location + '&format=json'
+    url = API_LINK + location + '&key=' + API_KEY
     response = urllib.request.urlopen(url)
     return json.loads(response.read())
 
-print(get_raw_data('brooklyn'))
+def get_coordinates(location):
+    data = get_raw_data(location)
+    return data['resourceSets'][0]['resources'][0]['point']['coordinates']
+    
+
+#print(get_raw_data('brooklyn'))
+print(get_coordinates('brooklyn'))
